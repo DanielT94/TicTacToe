4af3c68bb71fa05b623eec7515e67b92e1758050
DanielT94 <daniel.turnbull94@gmail.com>
2022-08-26 15:07:10 +0100

    Ran rubocop

--- main.rb
+++ main.rb
@@ -1,5 +1,5 @@
-PLAYER_ONE = "X"
-PLAYER_TWO = "O"
+PLAYER_ONE = 'X'
+PLAYER_TWO = 'O'
 WIN_COMBINATIONS = [
   [0, 1, 2], [3, 4, 5], [6, 7, 8],
   [0, 3, 6], [1, 4, 7], [2, 5, 8],
@@ -9,64 +9,63 @@ WIN_COMBINATIONS = [
 def initialize_board
   @count = 0
   @current_player = PLAYER_ONE
-  @board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  @board = %w[1 2 3 4 5 6 7 8 9]
   @game_over = false
 end
 
 def display_board(board)
-  puts ""
+  puts ''
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
+  puts '-----------'
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
+  puts '-----------'
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
-  puts ""
+  puts ''
 end
 
-
 def start_game
-  puts "Welcome to my 2 player Tic Tac Toe game."
-  puts "Player 1 will be X and Player 2 will be O."
-  puts "Select any available number from the grid to place your piece."
+  puts 'Welcome to my 2 player Tic Tac Toe game.'
+  puts 'Player 1 will be X and Player 2 will be O.'
+  puts 'Select any available number from the grid to place your piece.'
 end
 
 def place_piece
   puts "#{@current_player}, select your placement: "
   input = gets.chomp.to_i
-    if valid_move(input)
-      update_board(input)
-    else
-      puts "Invalid move, please select an available place."
-      place_piece
-    end
-end
-
-def position_taken_x?(input)
-  @board[input-1] == "X"
-end
-
-def position_taken_o?(input)
-  @board[input-1] == "O"
-end
-
-def valid_move(input)
-  !position_taken_x?(input) && !position_taken_o?(input) && input.between?(1, 9) 
-end
-
-def update_board(input)
-  @board[input -1] = @current_player
-end
-
-def switch_player
-  if(@current_player == PLAYER_ONE)
-    @current_player = PLAYER_TWO
+  if valid_move(input)
+    update_board(input)
   else
-    @current_player = PLAYER_ONE
+    puts 'Invalid move, please select an available place.'
+    place_piece
   end
 end
 
+def position_taken_x?(input)
+  @board[input - 1] == 'X'
+end
+
+def position_taken_o?(input)
+  @board[input - 1] == 'O'
+end
+
+def valid_move(input)
+  !position_taken_x?(input) && !position_taken_o?(input) && input.between?(1, 9)
+end
+
+def update_board(input)
+  @board[input - 1] = @current_player
+end
+
+def switch_player
+  @current_player = if @current_player == PLAYER_ONE
+                      PLAYER_TWO
+                    else
+                      PLAYER_ONE
+                    end
+end
+
 def turn_count
-  @count = @count + 1
+  @count += 1
   if @count >= 9
     @game_over = true
     puts "Game Over, it's a draw"
